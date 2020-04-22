25c86d3e683d33f501a7333056ed61e32ee934e4
samhuynhle <samhuynhle@gmail.com>
2020-04-22 00:13:22 -0700

    updating structure

---
+++ src/Components/Shared/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  
+  render() {
+    return(
+      <div className='footer'>
+
+      </div>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/Components/Shared/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div className='navbar'>
+
+      </div>
+    )
+  }
+}
+
+export default Navbar;
